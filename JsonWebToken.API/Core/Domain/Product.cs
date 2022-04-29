89b96160835f177149fdb53524fb71c7892c9c80
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-04-29 16:00:56 +0300

    added entities

--- JsonWebToken.API/Core/Domain/Product.cs
+++ JsonWebToken.API/Core/Domain/Product.cs
@@ -2,5 +2,15 @@
 {
     public class Product
     {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Stcok { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public Product()
+        {
+            Category = new Category();
+        }
     }
 }
