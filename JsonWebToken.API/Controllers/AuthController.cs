8d5cf769da8c5452216ec40745e4f20afa68f2e0
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-04-30 15:28:07 +0300

    added authController

---
+++ JsonWebToken.API/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JsonWebToken.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+    }
+}
